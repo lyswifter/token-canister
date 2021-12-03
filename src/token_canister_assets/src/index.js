d3cac87402559425e087e8b0ced7908a1b660686
lyswifter <ly70835@163.com>
2021-12-03 15:15:53 +0800

    add usdts utill, delete frontend canister

--- src/token_canister_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { token_canister } from "../../declarations/token_canister";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with token_canister actor, calling the greet method
-  const greeting = await token_canister.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
