3a39e847cd822920ce1df99fe4397a2f12b70f10
lyswifter <ly70835@163.com>
2021-12-20 19:44:51 +0800

    using ic-optimise

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cargo build --target wasm32-unknown-unknown --release --package token_canister && \
+ ic-cdk-optimizer ./target/wasm32-unknown-unknown/release/token_canister.wasm -o ./target/wasm32-unknown-unknown/release/token_canister.wasm
+
+#  cargo build --target wasm32-unknown-unknown --package chain_cloud --release
